1ad80af7f43d9d036a57419f8dc478c88a8a0f69
adi-nair07 <anairadithyan07@gmail.com>
2026-08-13 19:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.36%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    bool isPalindrome(int x) 
+    {
+        int temp=x;
+        long long rev=0;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x>0)
+        {
+            rev=rev*10+x%10;
+            x=x/10;
+        }
+        return temp==rev;
+    }
+};
